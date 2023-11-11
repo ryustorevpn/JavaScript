5d11de07d0debb597081a55e3aa80d8d59c1d7e1
=maulana <=animenationytb@gmail.com>
2023-11-11 12:02:57 +0700

    first commit

---
+++ Pertemuan 3/index.js
@@ -0,0 +1,18 @@
+mahasiswa = {
+    nama : 'Adit',
+    kelas : '1 SI B',
+    nim : 235720111026
+}
+console.log(mahasiswa)
+
+function udin(a, b){
+    return nama = " Jawaban (udin) = " + a * b;
+}
+console.log(udin(16, 7))
+
+function meong(a, b){
+    var total;
+    total = a * b;
+    return nama = " Hasil (meong) = " + total ;
+}
+console.log(meong(14, 7));
---
+++ Pertemunan 5/Operator.js
@@ -0,0 +1,25 @@
+const a = 61;
+const b = 42;
+
+const jumlah = a + b;
+const kurang = a - b;
+console.log (kurang);
+
+let angka = 10
+ angka *= 80;
+
+console.log ("hasil jumlah =",angka);
+
+
+// Hasil Dari Penugasan Kelas, Javascript
+var penugasan = "Kelas ";
+    penugasan += "Javascript"
+
+console.log(penugasan);
+
+
+var angkaa = "1"
+var angka1 = 1
+var angka2 = 2
+console.log("1.",angkaa == angka1)
+console.log("2.",angkaa === angka2)
