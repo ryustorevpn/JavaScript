ff770e7cde35bb912424ba4b5b80859c4f847c74
=maulana <=animenationytb@gmail.com>
2023-11-25 11:32:22 +0700

    JawaScript

---
+++ Pertemuan 7/main.js
@@ -0,0 +1,15 @@
+console.log("\n==================================================\n")
+
+//NOMOR 1
+for (let a = 1; a <= 20; a++){
+    console.log("Saya Berada Di Kelas KafeKoding Hari Ini (" + a + ")")
+}
+
+console.log("\n==================================================\n")
+
+//NOMOR 2
+for (let b = 0; b <= 15; b++){
+    console.log("║ Saya Belajar ke - " + b)
+}
+
+console.log("\n==================================================\n")
