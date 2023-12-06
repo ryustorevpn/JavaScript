7a3209f3c7d3f759eda6fd1defd6043eb1efeced
=maulana <=animenationytb@gmail.com>
2023-12-06 20:43:47 +0700

    JAVASCRIPT

--- Pertemuan 8/array_bersarang.js
+++ Pertemuan 8/array_bersarang.js
@@ -4,7 +4,7 @@ var negara = ["Indonesia", "Malaysia", "Singapura"];
 // Array bersarang untuk alamat peserta
 var alamatPeserta = [
     {
-        peserta: "Maulana Aditya",
+        peserta: "Maulana Aditya.",
         provinsi: "Sumatera Barat",
         kotaKabupaten: ["Padang", "Lima Puluh Kota", "Bukittinggi", "Payakumbuh"],
         kecamatan: ["Luhak", "Sijunjung", "Harau"],
@@ -14,9 +14,17 @@ var alamatPeserta = [
 ];
 
 // Menampilkan data peserta beserta alamat
-console.log("Nama saya " + alamatPeserta[0].peserta, ".Bertempat tinggal di negara " + negara[0]);
+console.log("Nama saya " + alamatPeserta[0].peserta, " Bertempat tinggal di negara " + negara[0]);
 console.log("Pada provinsi " + alamatPeserta[0].provinsi);
 console.log("Kabupaten " + alamatPeserta[0].kotaKabupaten[1]);
 console.log("Kecamatan " + alamatPeserta[0].kecamatan[0]);
 console.log("Jorong " + alamatPeserta[0].jorong[0]);
 console.log("Dan Jalan " + alamatPeserta[0].jalan);
+
+// Output
+Nama saya Maulana Aditya .Bertempat tinggal di negara Indonesia
+Pada provinsi Sumatera Barat
+Kabupaten Lima Puluh Kota
+Kecamatan Luhak
+Jorong Tabek Buruak
+Dan Jalan -
