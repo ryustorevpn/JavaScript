dc1cf63c66063d2a47ff6c805254dbba3f38264a
=maulana <=animenationytb@gmail.com>
2023-12-16 10:46:35 +0700

    JAVASCRIPT

---
+++ Pertemuan 9/index.js
@@ -0,0 +1,13 @@
+const mahasiswa1 = {
+    nama : 'udin',
+    kelas : '13A'
+}
+
+const mahasiswa2 = {
+    nama : 'bayu',
+    kelas : '69B',
+    umur : '69'
+}
+
+alert(mahasiswa2)
+console.log(mahasiswa2)
